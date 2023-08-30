db65b15b410515b598433f257de4cbd8050a6193
akatz6 <aaronk488@gmail.com>
2023-08-30 13:24:09 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      <button type="button" className="btn btn-primary">Primary</button>
+    </>
+  )
+}
+
+export default App
